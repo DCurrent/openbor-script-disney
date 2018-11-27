89425718182f1a23aab6f3ec24c6f9c474474fad
Caskey, Damon V <dc@caskeys.com>
2018-11-27 16:28:39 -0500

    Use default entity if set entity is invalid.

--- entity.c
+++ entity.c
@@ -2,18 +2,7 @@
 
 #import "data/scripts/dc_disney/instance.c"
 
-// Entity is the primary entity that performs actions.
-void dc_disney_set_entity(void value)
-{
-	int instance;
-	void result;
-
-	// Get instance.
-	instance = dc_disney_get_instance();
-
-	setlocalvar(instance + DC_DISNEY_VAR_KEY_ENT, value);
-}
-
+// Primary acting entity.
 void dc_disney_get_entity()
 {
 	int instance;
@@ -26,8 +15,19 @@ void dc_disney_get_entity()
 
 	if (typeof(result) != openborconstant("VT_PTR"))
 	{
-		result = getlocalvar("self");
+		result = DC_DISNEY_DEFAULT_ENT;
 	}
 
 	return result;
+}
+
+void dc_disney_set_entity(void value)
+{
+	int instance;
+	void result;
+
+	// Get instance.
+	instance = dc_disney_get_instance();
+
+	setlocalvar(instance + DC_DISNEY_VAR_KEY_ENT, value);
 }
