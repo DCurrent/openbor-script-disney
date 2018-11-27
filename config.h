88bf475aa4e8ab73475d3a571fccf13adc166b10
Caskey, Damon V <dc@caskeys.com>
2018-11-27 16:29:06 -0500

    Add support constants for animation variable.

--- config.h
+++ config.h
@@ -2,13 +2,15 @@
 #define DC_DISNEY_CONFIG 1
 
 #define DC_DISNEY_DEFAULT_INSTANCE		0
-#define DC_DISNEY_DEFAULT_ENT			getlocalvar("self");
+#define DC_DISNEY_DEFAULT_ANIMATION		openborconstant("ANI_IDLE")
+#define DC_DISNEY_DEFAULT_ENT			getlocalvar("self")
 
 // Variable keys. Used to identify cross function
 // variables used throughout library.
 #define DC_DISNEY_VAR_KEY_BASE_ID		"dcdis"	// Base name of every var ID. Must be unique vs. all other libraries.
 #define DC_DISNEY_VAR_KEY_INSTANCE		DC_DISNEY_VAR_KEY_BASE_ID + 0
-#define DC_DISNEY_VAR_KEY_ENT			DC_DISNEY_VAR_KEY_BASE_ID + 1
+#define DC_DISNEY_VAR_KEY_ANIMATION		DC_DISNEY_VAR_KEY_BASE_ID + 1
+#define DC_DISNEY_VAR_KEY_ENT			DC_DISNEY_VAR_KEY_BASE_ID + 2
 #define DC_DISNEY_VAR_KEY_THE_END		2			// Should always be last, with a value one higher than previous key ID.
 
 #endif // !DC_DISNEY_CONFIG
