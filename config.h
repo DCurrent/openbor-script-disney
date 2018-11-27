187bae8a213559ed7983eb0c9572be9b93d01586
Caskey, Damon V <dc@caskeys.com>
2018-11-27 12:13:35 -0500

    Add base name concatenated constants.

--- config.h
+++ config.h
@@ -1,8 +1,13 @@
 #ifndef DC_DISNEY_CONFIG
-
 #define DC_DISNEY_CONFIG 1
 
-#define DC_DISNEY_VAR_KEY_ENT "dcdis_0"
+
+// Variable keys. Used to identify cross function
+// variables used throughout library.
+#define DC_DISNEY_VAR_KEY_BASE_ID		"dcdis"	// Base name of every var ID. Must be unique vs. all other libraries.
+#define DC_DISNEY_VAR_KEY_INSTANCE		DC_DISNEY_VAR_KEY_BASE_ID + 0
+#define DC_DISNEY_VAR_KEY_ENT			DC_DISNEY_VAR_KEY_BASE_ID + 1
+#define DC_DISNEY_VAR_KEY_THE_END		2			// Should always be last, with a value one higher than previous key ID.
 
 #endif // !DC_DISNEY_CONFIG
 
