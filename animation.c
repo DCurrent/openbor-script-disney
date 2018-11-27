f88cbef533824fd219ade89101f5fb893ed01ab4
Caskey, Damon V <dc@caskeys.com>
2018-11-27 16:29:29 -0500

    Add animation access and mutator.

---
+++ animation.c
@@ -0,0 +1,33 @@
+#include "data/scripts/dc_disney/config.h"
+
+#import "data/scripts/dc_disney/instance.c"
+
+// Animation to apply.
+void dc_disney_get_animation()
+{
+	int instance;
+	int result;
+
+	// Get instance.
+	instance = dc_disney_get_instance();
+
+	result = getlocalvar(instance + DC_DISNEY_VAR_KEY_ANIMATION);
+
+	if (typeof(result) != openborconstant("VT_INTEGER"))
+	{
+		result = DC_DISNEY_DEFAULT_ANIMATION;
+	}
+
+	return result;
+}
+
+void dc_disney_set_animation(void value)
+{
+	int instance;
+	
+	// Get instance.
+	instance = dc_disney_get_instance();
+
+	setlocalvar(instance + DC_DISNEY_VAR_KEY_ANIMATION, value);
+}
+
